08f3a4052cd87142b8d4287b355c31797ca679db
jamesmf <jamesmfrick@gmail.com>
2017-09-21 23:24:44 -0400

    textures sorry github

--- Board.gd
+++ Board.gd
@@ -1,11 +1,32 @@
 extends Container
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var boardSize = 10
+var xMargin = 20
+var yMargin = 20
+var grid = []
+var minSize
+var hexConvert = 2/sqrt(3)
+
+func populateGrid():
+	var sizes = get_size()
+	print(sizes)
+	#minSize = min(sizes[0]-2*xMargin, sizes[1]-2*yMargin)/boardSize
+	minSize = 128
+	for i in range(0,boardSize):
+		var row = []
+		for j in range(0,boardSize):
+			row.append([xMargin+hexConvert*minSize*i,yMargin+hexConvert*minSize*j])
+		grid.append(row)
+
 
 func _ready():
 	# Called every time the node is added to the scene.
+	populateGrid()
+	print(minSize)
 	var hexScene = preload("res://Hex1.tscn")
 	var hex = hexScene.instance()
+	print(grid[0][0],grid[1][0],grid[1][1])
+	var x = grid[0][0][0]
+	var y = grid[0][0][1]
+	hex._place(x, y, minSize)
 	add_child(hex)
--- basicGUI.gd
+++ basicGUI.gd
@@ -1,35 +1,20 @@
 extends TextureFrame
 
-var tapped=false
-
-#func blah(origin, r):
-#	var myArray = Vector2Array()
-#	for i in range(0,6):
-#		var vec = origin + Vector2(r * cos(2 * PI * i / 6), r * sin(2 * PI * i / 6))
-#		myArray.append(vec)
-#	return myArray
-	
+var placed = false
 	
 func _draw():
-#	var array = blah(Vector2(50,50),20)
-#	var color = Color(1, 0, 0)
-	#draw_colored_polygon(array, color)
-    #var r = Rect2( Vector2(), get_size() )
-	if (tapped):
-#		color = Color(1, 0, 0)
-#		draw_colored_polygon(array, color)
-		var newPos = Vector2(randf(), randf())
-		print(newPos)
-		set_pos(newPos)
+	if placed:
 		update()
-		tapped = false
-#	else:
-#		color = Color(0, 1, 0)
-		#draw_colored_polygon(array, color)
+
+
+func _place(i, j, size):
+	set_pos(Vector2(i,j))
+	set_size(Vector2(size, size))
+	print(get_size())
+	placed = true
 
 
 func _input_event(ev):
 	if (ev.type==InputEvent.MOUSE_BUTTON and ev.pressed):
 		print("hello!")
-		tapped = not tapped
 		update()
